ae62547bd9f2966c8803ace803d8ae4f9bf9e287
Dalso13 <flskwhdhk@gmail.com>
2025-03-18 11:58:02 +0900

    feat: 뒤로가기 제어

--- app/src/main/java/com/jdw/listview/MainActivity.kt
+++ app/src/main/java/com/jdw/listview/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.jdw.listview
 
 import android.os.Bundle
+import android.os.Handler
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.jdw.listview.adapter.ListViewAdapter
 import com.jdw.listview.viewModel.ListViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private var isDouble = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +31,19 @@ class MainActivity : AppCompatActivity() {
         // 리스트뷰 어뎁터 적용
         listview.adapter = adapter
 
+
+    }
+
+    override fun onBackPressed() {
+
+        if (isDouble) super.onBackPressed()
+
+        isDouble = true
+        Toast.makeText(this, "종료하시려면 더블클릭", Toast.LENGTH_LONG).show()
+
+        Handler().postDelayed(Runnable {
+            isDouble = false
+        }, 2000)
 
     }
 }
